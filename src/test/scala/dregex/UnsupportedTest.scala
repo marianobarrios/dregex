5a273cfb7aa24dd38c4ef42ae03ace370400e2f3
Mariano Barrios <marbar@gmail.com>
2015-01-29 01:07:27 -0300

    Add test

---
+++ src/test/scala/dregex/UnsupportedTest.scala
@@ -0,0 +1,26 @@
+package dregex
+
+import org.scalatest.FunSuite
+
+class UnsupportedTest extends FunSuite {
+
+  test("trailing lookahead") {
+    intercept[UnsupportedException](Regex.compile("a(?!a)"))
+  }
+
+  test("deep lookaround") {
+    intercept[UnsupportedException](Regex.compile("(?!a)|b"))
+    intercept[UnsupportedException](Regex.compile("(?!a(?!b))"))
+    intercept[UnsupportedException](Regex.compile("b((?!a)c)"))
+  }
+
+  test("lookbehind") {
+    intercept[UnsupportedException](Regex.compile("a(?<!b)c"))
+    intercept[UnsupportedException](Regex.compile("a(?<=b)c"))
+    intercept[UnsupportedException](Regex.compile("(?<!b)c"))
+    intercept[UnsupportedException](Regex.compile("(?<=b)c"))
+    intercept[UnsupportedException](Regex.compile("a(?<!b)"))
+    intercept[UnsupportedException](Regex.compile("a(?<=b)"))
+  }
+
+}
