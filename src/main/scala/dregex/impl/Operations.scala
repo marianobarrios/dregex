5a6e6e5eb07c543629a5101a3c60e34f877153b5
Mariano Barrios <marbar@gmail.com>
2015-02-02 09:24:42 -0300

    Do selected minimization

--- src/main/scala/dregex/impl/Operations.scala
+++ src/main/scala/dregex/impl/Operations.scala
@@ -2,18 +2,26 @@ package dregex.impl
 
 import dregex.impl.MetaDfas.MetaDfa
 import dregex.impl.MetaDfas.DfaOperation
-import dregex.impl.MetaDfas.DfaOperation
 import dregex.impl.MetaDfas.AtomDfa
 
 object Operations {
 
+  /**
+   * Minimization is expensive, so it is done if the number of states surpasses a a threshold. The exact number just 
+   * happened to work in practice.
+   */
+  val minimizationThreshold = 50
+  
   def resolve(abstractDfa: MetaDfa): Dfa = {
     val resolved = abstractDfa match {
       case DfaOperation(Operation.Intersect, left, right) => resolve(left) intersect resolve(right)
       case DfaOperation(Operation.Substract, left, right) => resolve(left) diff resolve(right)
-      case atom: AtomDfa => atom.dfa
+      case atom: AtomDfa => atom.dfa.minimize()
     }
-    resolved.minimize()
+    if (resolved.impl.allStates.size >= minimizationThreshold)
+      resolved.minimize()
+    else 
+      resolved
   }
 
   object Operation extends Enumeration {
