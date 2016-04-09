7d3bef826002a52c8ba4683eeb91cbcb56cb4de3
Mariano Barrios <marbar@gmail.com>
2016-04-09 10:28:38 +0200

    Make performance test more strict

--- src/test/scala/dregex/PerformanceTest.scala
+++ src/test/scala/dregex/PerformanceTest.scala
@@ -4,23 +4,24 @@ import org.scalatest.FunSuite
 import dregex.impl.Util
 
 class PerformanceTest extends FunSuite {
-  
-    test("slow regexs") {
-      val (regexes, elapsed1) = Util.time {
-        Regex.compile(Seq(
-            "qwertyuiopasd",
-            "/aaaaaa/(?!xxc)(?!xxd)(?!xxe)(?!xxf)(?!xxg)(?!xxh).*",
-            "/aaaaaa/(?!x+c)(?!x+d)(?!x+e)(?!x+f)(?!x+g)(?!x+h).*",
-            "/aaaaaa/(?!x+c|x+d|x+e|x+f|x+g|x+h).*",
-            "/aaaaaa/(?!xxc)a(?!xxd)b(?!xxe)c(?!xxf).*", // disables lookahead combinations
-            "/aaaaaa/(?!xxc|xxd|xxe|xxf|xxg|xxh).*"
-          )).unzip._2
-      }
-      info(s"compilation time: ${elapsed1 / 1000} ms")
-      val (_, elapsed2) = Util.time {
-        regexes.tail.foreach(_ doIntersect regexes.head)
-      }
-      info(s"intersection time: ${elapsed2 / 1000} ms")
+
+  test("slow regexs") {
+    val (regexes, elapsed1) = Util.time {
+      Regex.compile(Seq(
+        "qwertyuiopasd",
+        "/aaaaaa/(?!xxc)(?!xxd)(?!xxe)(?!xxf)(?!xxg)(?!xxh)[a-zA-Z0-9]{7}.*",
+        "/aaaaaa/(?!x+c)(?!x+d)(?!x+e)(?!x+f)(?!x+g)(?!x+h)[a-zA-Z0-9]{7}.*",
+        "/aaaaaa/(?!x+c|x+d|x+e|x+f|x+g|x+h)[a-zA-Z0-9]{7}.*",
+        "/aaaaaa/(?!xxc)a(?!xxd)b(?!xxx)c(?!xxf)[a-zA-Z0-9]{7}.*", // disables lookahead combinations
+        "/aaaaaa/(?!xxc|xxd|xxe|xxf|xxg|xxh)[a-zA-Z0-9]{7}.*",
+        "/aaaaaa/(?!xxc.*)(?!xxd.*)(?!xxe.*)(?!xxf.*)(?!xxg.*)[a-zA-Z0-9]{7}.*"
+        )).unzip._2
     }
+    info(s"compilation time: $elapsed1")
+    val (_, elapsed2) = Util.time {
+      regexes.tail.foreach(_ doIntersect regexes.head)
+    }
+    info(s"intersection time: $elapsed2")
+  }
 
 }
