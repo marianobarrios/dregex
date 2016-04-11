56cf28ad5ee71fbacaeb555ae79ac9387b37394d
Mariano Barrios <marbar@gmail.com>
2016-04-11 20:35:36 +0200

    Add some comparison with pathological backtracking cases

--- src/test/scala/dregex/PerformanceTest.scala
+++ src/test/scala/dregex/PerformanceTest.scala
@@ -2,6 +2,7 @@ package dregex
 
 import org.scalatest.FunSuite
 import dregex.impl.Util
+import java.util.regex.Pattern
 
 class PerformanceTest extends FunSuite {
 
@@ -22,6 +23,25 @@ class PerformanceTest extends FunSuite {
       regexes.tail.foreach(_ doIntersect regexes.head)
     }
     info(s"intersection time: $elapsed2")
+  }
+  
+  private def compare(regex: String, text: String, shouldMatch: Boolean) {
+    info(s"matching '$text' with /$regex/")
+    val nfa = Pattern.compile(regex)
+    val dfa = Regex.compile(regex)
+    val (_, nfaElapsed) = Util.time {
+      assertResult(shouldMatch)(nfa.matcher(text).matches())
+    }
+    info(s"NFA (java.util.regex) time: $nfaElapsed")
+    val (_, dfaElapsed) = Util.time {
+      assertResult(shouldMatch)(dfa.matches(text))
+    }
+    info(s"DFA (dregex) time: $dfaElapsed")
+  }
+  
+  test("NFA comparison") {
+    compare(regex = "(x+x+)+y", text = "xxxxxxxxxxxxxxxxxxxxxxxxxxxx", shouldMatch = false)
+    compare(regex = "(.*?,){27}P", text = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27", shouldMatch = false)
   }
 
 }
