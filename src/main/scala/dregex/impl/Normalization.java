f5e8aafa2fb008f554d8d80cf180c5aeb3a5bc15
Mariano Barrios <marbar@gmail.com>
2023-04-07 14:01:02 +0200

    Migrate to Java: Normalization

---
+++ src/main/scala/dregex/impl/Normalization.java
@@ -0,0 +1,44 @@
+package dregex.impl;
+
+public enum Normalization implements Normalizer {
+
+    NoNormalization {
+        @Override
+        public CharSequence normalize(CharSequence str) {
+            return str;
+        }
+    },
+
+    LowerCase {
+        @Override
+        public CharSequence normalize(CharSequence str) {
+            var builder = new StringBuilder(str.length());
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                boolean isAsciiUpperCase = ch >= 'A' && ch <= 'Z';
+                builder.append(isAsciiUpperCase ? ch + 0x20 : ch);
+            }
+            return builder.toString();
+        }
+    },
+
+    UnicodeLowerCase {
+        @Override
+        public CharSequence normalize(CharSequence str) {
+            var builder = new StringBuilder();
+            str.codePoints().forEach(codePoint -> {
+                builder.append(Character.toLowerCase(codePoint));
+            });
+            return builder.toString();
+        }
+    },
+
+    CanonicalDecomposition {
+        @Override
+        public CharSequence normalize(CharSequence str) {
+            return java.text.Normalizer.normalize(str, java.text.Normalizer.Form.NFD);
+        }
+    };
+
+}
+
