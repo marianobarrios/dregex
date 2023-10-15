1c0e4ae450403b451a603ee209315802826cec1c
Mariano Barrios <marbar@gmail.com>
2023-10-15 18:53:02 +0200

    Migrate to Java: RangeOps

--- src/test/scala/dregex/RangeOpsTest.scala
+++ src/test/scala/dregex/RangeOpsTest.scala
@@ -4,6 +4,8 @@ import dregex.impl.RangeOps
 import org.scalatest.funsuite.AnyFunSuite
 import dregex.impl.tree.CharRange
 
+import scala.jdk.CollectionConverters._
+
 class RangeOpsTest extends AnyFunSuite {
 
   implicit def pairToRange(pair: (Int, Int)): CharRange = {
@@ -14,8 +16,8 @@ class RangeOpsTest extends AnyFunSuite {
 
   test("union") {
     val ranges = Seq[CharRange]((10, 20), (9, 9), (25, 28), (3, 3), (10, 11), (9, 10), (100, 100), (101, 101))
-    val union = RangeOps.union(ranges.sortBy(x => (x.from, x.to)))
-    val expected: Seq[CharRange] = Seq((3, 3), (9, 20), (25, 28), (100, 101))
+    val union = RangeOps.union(ranges.sortBy(x => (x.from, x.to)).asJava)
+    val expected = Seq[CharRange]((3, 3), (9, 20), (25, 28), (100, 101)).asJava
     assertResult(expected)(union)
   }
 
