5a1780f4bf95024b26c32ff2a72eeb605b38d836
Mariano Barrios <marbar@gmail.com>
2023-10-15 18:54:46 +0200

    Migrate to java: package info

--- src/main/scala/package-info.java
+++ src/main/scala/package-info.java
@@ -2,4 +2,4 @@
   * Main package for Dregex implementation. User-facing classes reside directly in the top-level package. The rest of
   * the sub-packages contain implementation classes.
   */
-package object dregex
+package dregex;
