878c91775f40413eac723184acda58defdd2a561
Mariano Barrios <marbar@gmail.com>
2023-10-17 22:54:37 +0200

    Add serialversionid

--- src/main/java/dregex/InvalidRegexException.java
+++ src/main/java/dregex/InvalidRegexException.java
@@ -1,6 +1,11 @@
 package dregex;
 
+import java.io.Serial;
+
 public class InvalidRegexException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1;
 
     public InvalidRegexException(String message) {
         super(message);
