58fb7320ad11b11322167051634fb52c62f48458
Mariano Barrios <marbar@gmail.com>
2023-10-17 22:58:54 +0200

    Remove java.io.Serial to work in Java 11

--- src/main/java/dregex/InvalidRegexException.java
+++ src/main/java/dregex/InvalidRegexException.java
@@ -1,10 +1,7 @@
 package dregex;
 
-import java.io.Serial;
-
 public class InvalidRegexException extends RuntimeException {
 
-    @Serial
     private static final long serialVersionUID = 1;
 
     public InvalidRegexException(String message) {
